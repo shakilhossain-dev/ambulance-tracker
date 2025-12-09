105a0d60a38f689053dad2eb8fab9c8cd316c0b0
24742104 <24742104@edu.cc.saga-u.ac.jp>
2025-12-09 21:05:29 +0900

    comment add

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ambulance_tracker/Animation/FadeAnimation.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/Welcome/welcome_screen.dart';
+//THis is a comment
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
